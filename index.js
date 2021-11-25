32382c0b4f0d7fdef4d01995c11a10b0a0884425
towhidJ <towhidulislamcse@gmail.com>
2021-11-25 23:19:40 +0600

    add appointments status change

--- index.js
+++ index.js
@@ -19,21 +19,75 @@ async function run() {
         await client.connect();
         const database = client.db('medi-care');
         const appionmentsCollection = database.collection('appionments');
+        const usersCollection = database.collection('users');
+
 
 
         //Get appionments
 
-        app.get ('/appionments',async (req,res)=>{
+        app.get ('/appointments',async (req,res)=>{
           const cursor = appionmentsCollection.find({});
           const appionments = await cursor.toArray();
           res.json(appionments);
         })
 
+        //Get appionments by email
+
+        app.get ('/appointments/email',async (req,res)=>{
+          const email = req.body;
+          const query = {email:email}
+          const cursor = appionmentsCollection.find(query);
+          const appionments = await cursor.toArray();
+          res.json(appionments);
+        })
+
         //Add Appointment
-        app.post('/appionments',async (req,res)=>{
+        app.post('/appointments',async (req,res)=>{
+
           const appionment = req.body;
+          console.log(appionment);
           const result = await appionmentsCollection.insertOne(appionment);
           res.json(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            user.role = 'user';
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const  requester = user.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (!requesterAccount)
+            {
+                user.role = 'user';
+            }
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+
+
+        });
+
+        //Updata Order status
+        app.put('/status/:id',async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const data = req.body;
+            const updateDoc = {
+                $set: {
+                    status: data.status
+                },
+            };
+            console.log(data);
+            const order = await appionmentsCollection.updateOne(query,updateDoc);
+            res.json(order);
         })
 
     }
